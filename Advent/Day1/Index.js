22cdd057a367d01d1eca23066c428228448dc677
PlatoXL <andy.lycett@gmail.com>
2020-12-20 00:37:53 +0100

    commit on 'main'

---
+++ Advent/Day1/Index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const list = fs.readFileSync('input.txt', 'utf8').split('\r\n').slice(0, -1)
+
+
+list.forEach((row, i) => {
+    var TermOne = list[i]
+        for (let j = 0; j < list.length; j++) {
+            var TermTwo = list[j];
+                if (TermOne + TermTwo == "2020") {
+                        console.log(TermOne);
+                        console.log(TermTwo);
+                    
+                }
+            
+        }
+
+    
+});
