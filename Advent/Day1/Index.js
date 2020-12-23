9c5522ca7ddbb4a051ab7c56549e7098c3535155
PlatoXL <andy.lycett@gmail.com>
2020-12-23 23:45:10 +0100

    commit

--- Advent/Day1/Index.js
+++ Advent/Day1/Index.js
@@ -1,16 +1,19 @@
 const fs = require('fs')
-const list = fs.readFileSync('input.txt', 'utf8').split('\r\n').slice(0, -1)
+const list = fs.readFileSync('input.txt', 'utf8').split('\n').slice(0, -1)
 
 const nice = "nice"
 console.log(nice)
 
-list.forEach((row, i) => {
-    var TermOne = list[i]
+list.forEach(row => {
+    const TermOne = row
         for (let j = 0; j < list.length; j++) {
-                if (number(TermOne) + number(list[j]) == "2020") {
-                        console.log(TermOne);
-                        console.log(list[j]);
-                        console.log("very nice");
+               let termtwo = list[j]
+                for (let k = 0; k < list.length; k++) {
+                        const termthree = list[k];
+                                if (Number(TermOne) + Number(termtwo) + Number(termthree) === 2020) {
+                                        console.log(Number(TermOne) * Number(termtwo) * Number(termthree))
+                                }
+                        
                 }
             
         }
--- Advent/Day6/Index.js
+++ Advent/Day6/Index.js
@@ -1,1 +1,4 @@
+const fs = require('fs')
+const list = fs.readFileSync('input.txt', 'utf8').split("\n").slice(0, -1)
 
+console.log(list)
