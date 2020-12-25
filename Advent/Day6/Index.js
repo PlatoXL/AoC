5edc3e8591aec04110a3a929829fdc9274e79725
PlatoXL <andy.lycett@gmail.com>
2020-12-25 15:46:47 +0100

    commit

--- Advent/Day6/Index.js
+++ Advent/Day6/Index.js
@@ -1,4 +1,11 @@
 const fs = require('fs')
-const list = fs.readFileSync('input.txt', 'utf8').split("\n").slice(0, -1)
+const list = fs.readFileSync('input.txt', 'utf8').split("\r\n\r\n")
 
-console.log(list)
+var rownum = 0
+
+list.forEach(row => {
+    list[rownum].replace("\r\n", "")
+    console.log(list[rownum])
+    rownum ++
+
+});
