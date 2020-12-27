aa3e41755c73f15d663c9afaa7c941cc3ba74cd3
PlatoXL <andy.lycett@gmail.com>
2020-12-27 01:26:50 +0100

    commit

--- Advent/Day6/Index.js
+++ Advent/Day6/Index.js
@@ -1,24 +1,39 @@
-const fs = require('fs')
-const list = fs.readFileSync('input.txt', 'utf8').split("\r\n\r\n")
+const fs = require("fs");
+const list = fs.readFileSync("input.txt", "utf8").split("\r\n\r\n");
 
 function part1() {
-let bigboi = 0
-list.forEach((row , rownum)=> {
+  let bigboi = 0;
+  list.forEach((row, rownum) => {
     const answers = new Set();
-    row = row.replace(/\r\n/g, "")
-            let babylove = row.split('')
-                babylove.forEach(baby => {
-                    answers.add(baby)
-
-                })
-bigboi += answers.size
-});
-return bigboi
+    row = row.replace(/\r\n/g, "");
+    let babylove = row.split("");
+    babylove.forEach((baby) => {
+      answers.add(baby);
+    });
+    bigboi += answers.size;
+  });
+  return bigboi;
 }
 
-console.log(part1())
+console.log(part1());
 
-list.forEach((row , rownum)=> {
-    let person = row.split('\r\n')
-    console.log(person)
-});
+function part2() {
+  let output = 0;
+  list.forEach((row, rownum) => {
+    let persons = row.split("\r\n");
+    const forekomst = {};
+    persons.forEach((person, j) => {
+      let chars = person.split("");
+      chars.forEach((char) => {
+        forekomst[char] = (forekomst[char] || 0) + 1;
+      });
+    });
+    Object.values(forekomst).forEach((value) => {
+      if (value == persons.length) {
+        output++;
+      }
+    });
+  });
+  return output;
+}
+console.log(part2());
