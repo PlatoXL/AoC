fa0612de051edc6302fd120f4768ae28adcd4985
PlatoXL <andy.lycett@gmail.com>
2020-12-27 03:05:01 +0100

    les go

--- Advent/Day4/index.js
+++ Advent/Day4/index.js
@@ -2,9 +2,26 @@ const fs = require("fs");
 const list = fs.readFileSync("input.txt", "utf8").split("\r\n\r\n");
 
 let valid = 0;
-const req = "byr, iyr, eyr, hgt, hcl, ecl, pid";
+const req = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 
-list.forEach((passport) => {
-  let field = passport.split(/\r\n| /);
-  console.log(field);
-});
+function part1() {
+  list.forEach((passport) => {
+    let fields = passport.split(/\r\n| /);
+    let numcorrectfield = 0;
+    fields.forEach((field) => {
+      for (let i = 0; i < 7; i++) {
+        let reqtest = new RegExp(req[i]);
+        if (reqtest.test(field) == true) {
+          numcorrectfield++;
+        }
+      }
+      if (numcorrectfield == 7) {
+        valid++;
+      }
+    });
+  });
+
+  return valid;
+}
+
+console.log(part1());
