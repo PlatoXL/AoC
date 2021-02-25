a62539f00071a8ca99dbfb1417973a019f6b6b1e
PlatoXL <andy.lycett@gmail.com>
2021-02-25 01:30:42 +0100

    jadå

--- Advent/Day14/index.js
+++ Advent/Day14/index.js
@@ -1,22 +1,23 @@
 const fs = require("fs");
 const list = fs.readFileSync("input.txt", "utf8").split("\n");
 const earliesttime = Number(list[0]);
-let temp = list[1].split(/(?<=\d),/g);
-const busids = temp.map((busiq) => {
+const busids = list[1].split(/(?<=\d),/g).map((busiq) => {
   return Number(busiq.replace(/\D+/g, ""));
 });
-console.log(busids);
+function part1() {
+  let answers = [];
+  let pallarinteforloop = 0;
+  busids.forEach((busid) => {
+    let time = 0;
+    while (time < earliesttime) {
+      time = busid + time;
+    }
+    answers[pallarinteforloop] = time - earliesttime;
+    pallarinteforloop++;
+  });
+  let min = Math.min(...answers);
+  let answer = min * busids[answers.indexOf(min)];
+  return answer;
+}
 
-const answers = [];
-let pallarinteforloop = 0;
-busids.forEach((busid) => {
-  let time = 0;
-  while (time < earliesttime) {
-    time = busid + time;
-  }
-  answers[pallarinteforloop] = time - earliesttime;
-  pallarinteforloop++;
-});
-let min = Math.min(...answers);
-let answer = min * busids[answers.indexOf(min)];
-console.log(answer);
+console.log(part1());
